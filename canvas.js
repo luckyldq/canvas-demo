6fc6bc7ea239549300b41616ee7baf1487832bbb
ldqlucky <1195212512@qq.com>
2022-10-23 19:20:09 +0800

    修改canvas.js设置项

--- canvas.js
+++ canvas.js
@@ -1,10 +1,11 @@
 /* 
 设置主题，如
-color： 颜色
-type: 描边或填充
+style： 颜色
+注释的属性：在调用时传入即可
 */
 let options = {
-    color:"black",
+    color:'black', // 颜色
+    style:"black", // fillStyle或者strokeStyle参数
     font:"20px 宋体",
     textAlign:"center",
     textBaseline:"middle",
@@ -12,9 +13,12 @@ let options = {
     lineCap: "butt",
     lineJoin: "miter",
     r:5,
-    type:'stroke',
-    
-    // globalCompositeOperation:"source-over"
+    // type:'stroke',  // 考虑到可能会同时描边和填充的情况，扩展成isStroke和isFill
+    isStroke:true,  // 默认描边
+    isFill:false    // 是否填充
+    // style:'', // 颜色，渐变，图案
+    // globalCompositeOperation:"source-over"  // 图像混合模式
+    // extendStyle:function(){}  // 用来扩展其他样式
 }
 function setTheme(themeOptions){
     options = Object.assign({}, options, themeOptions);
@@ -44,13 +48,13 @@ x1, y1：绘制终点
 function strokeLine(extendOptions){
     let option = {
         // ctx, x0, y0, x1, y1, 
-        color: options.color,
+        style: options.style,
         lineWidth: options.lineWidth,
         // lineCap: options.lineCap,
         // lineJoin: options.lineJoin
     }
-    let {ctx, x0, y0, x1, y1, color, lineWidth, lineCap, lineJoin, globalCompositeOperation} = Object.assign({},option,extendOptions);
-    ctx.strokeStyle = color;
+    let {ctx, x0, y0, x1, y1, style, lineWidth, lineCap, lineJoin, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    ctx.strokeStyle = style || style;
     ctx.lineWidth = lineWidth;
     lineCap && (ctx.lineCap = lineCap);
     lineJoin && (ctx.lineJoin = lineJoin);
@@ -69,16 +73,16 @@ x1, y1：绘制终点
 function strokeDashLine(extendOptions){
     let option = {
         // ctx, x0, y0, x1, y1, 
-        color: options.color,
+        style: options.style,
         dash: [3,3],
         lineWidth: options.lineWidth
     }
-    let {ctx, x0, y0, x1, y1, color, dash, lineWidth, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    let {ctx, x0, y0, x1, y1, style, dash, lineWidth, globalCompositeOperation} = Object.assign({},option,extendOptions);
     ctx.beginPath();
     ctx.setLineDash(dash);
     ctx.moveTo(x0, y0);
     ctx.lineTo(x1, y1);
-    ctx.strokeStyle = color;
+    ctx.strokeStyle = style;
     ctx.lineWidth = lineWidth;
     _setComposite(ctx, globalCompositeOperation);
     ctx.stroke();
@@ -87,42 +91,49 @@ function strokeDashLine(extendOptions){
 }
 /* 
 绘制文字
-type: stroke  fill
 */
 function drawText(extendOptions){
     let option = {
-        // ctx, text, x, y, 
-        type: options.type,
+        // ctx, text, x, y,
         font: options.font,
         textAlign: options.textAlign,
         textBaseline: options.textBaseline,
-        color:options.color
+        style:options.style,
+        isStroke:options.isStroke,
+        isFill:options.isFill
     }
-    let {ctx, text, x, y, font, type, textAlign, textBaseline, color, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    let {ctx, text, x, y, font, isStroke, isFill, textAlign, textBaseline, style, globalCompositeOperation} = Object.assign({},option,extendOptions);
     ctx.beginPath();
     ctx.font = font;
     ctx.textAlign = textAlign;
     ctx.textBaseline = textBaseline;
     _setComposite(ctx, globalCompositeOperation);
     // fillText  strokeText
-    ctx[type + 'Style'] = color;
-    ctx[type+'Text'](text,  x, y);
+    if(isStroke){
+        ctx.strokeStyle = style;
+        ctx.strokeText(text,  x, y);
+    }
+    if(isFill){
+        ctx.fillStyle = style;
+        ctx.fillText(text,  x, y);
+    }
 }
 /* 
 绘制填充矩形
 */
 function drawRect(extendOptions){
     let option = {
-        // ctx, x, y, w, h, 
-        type: options.type, 
-        color: options.color
+        // ctx, x, y, w, h,
+        style: options.style,
+        isStroke:options.isStroke,
+        isFill:options.isFill
     }
-    let {ctx, x, y, w, h, type, color, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    let {ctx, x, y, w, h, isStroke, isFill, style, globalCompositeOperation} = Object.assign({},option,extendOptions);
     ctx.beginPath();
     ctx.rect(x, y, w, h);
     _setComposite(ctx, globalCompositeOperation);
-    // 动态函数名优化
-    type=='fill'?_fillGraph(ctx, color):_strokeGraph(ctx, color);
+    isStroke && _strokeGraph(ctx, style);
+    isFill && _fillGraph(ctx, style);
 }
 /* 
 绘制扇形
@@ -131,16 +142,17 @@ function drawSector(extendOptions){
     let option = {
         // ctx, x, y, r, deg1, deg2, 
         r:options.r,
-        type: options.type, 
-        color: options.color,
-        counterclockwise: false
+        style: options.style,
+        counterclockwise: false,
+        isStroke:options.isStroke,
+        isFill:options.isFill
     }
-    let {ctx, x, y, r, deg1, deg2, counterclockwise, type, color, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    let {ctx, x, y, r, deg1, deg2, counterclockwise, isStroke, isFill, style, globalCompositeOperation} = Object.assign({},option,extendOptions);
     ctx.beginPath();
     ctx.arc(x, y, r, deg1, deg2, counterclockwise);
     _setComposite(ctx, globalCompositeOperation);
-    // 动态函数名优化
-    type=='fill'?_fillGraph(ctx, color):_strokeGraph(ctx, color);
+    isStroke && _strokeGraph(ctx, style);
+    isFill && _fillGraph(ctx, style);
 }
 /* 
 绘制圆形
@@ -149,15 +161,16 @@ function drawCircle(extendOptions){
     let option = {
         // ctx, x, y, 
         r: options.r,
-        type: options.type, 
-        color: options.color
+        style: options.style,
+        isStroke:options.isStroke,
+        isFill:options.isFill
     }
-    let {ctx, x, y, r, type, color, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    let {ctx, x, y, r, isStroke, isFill, style, globalCompositeOperation} = Object.assign({},option,extendOptions);
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2*Math.PI, false);
     _setComposite(ctx, globalCompositeOperation);
-    // 动态函数名优化
-    type=='fill'?_fillGraph(ctx, color):_strokeGraph(ctx, color);
+    isStroke && _strokeGraph(ctx, style);
+    isFill && _fillGraph(ctx, style);
 }
 /* 
 设置图形叠加模式
@@ -168,26 +181,48 @@ function _setComposite(ctx, globalCompositeOperation){
 /* 
 描边图形
 */
-function _strokeGraph(ctx, color){
-    ctx.strokeStyle = color;
+function _strokeGraph(ctx, style){
+    ctx.strokeStyle = style;
     ctx.stroke();
 }
 /* 
 填充图形
 */
-function _fillGraph(ctx, color){
-    ctx.fillStyle = color;
+function _fillGraph(ctx, style){
+    ctx.fillStyle = style;
     ctx.fill();
+}
+/////////// 绘制全局效果 ////////////////////
+/* 
+绘制阴影比较消耗浏览器性能
+x：水平偏移
+y：垂直偏移
+style：阴影颜色
+blur：阴影模糊度
+*/
+function setShadowStyle(extendOptions){
+    let option = {
+        // ctx,x,y,blur
+        x:5,
+        y:5,
+        style:options.style,
+        blur: 5
+    }
+    let {ctx, x, y, style, blur, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    ctx.shadowstyle = style;
+    ctx.shadowOffsetX = x;
+    ctx.shadowOffsetY = y;
+    ctx.shadowBlur = blur;
 }
 /////////// 绘制复杂业务图形 ////////////////
 /* 
-绘制十字线
+绘制某个坐标的十字线
 */
 function drawCrossLine(extendOptions){
     let option = {
         // ctx, x, y
     }
-    let {ctx, x, y, color, lineWidth, globalCompositeOperation} = Object.assign({},option,extendOptions);
+    let {ctx, x, y, style, lineWidth, globalCompositeOperation} = Object.assign({},option,extendOptions);
     // 横向
     strokeLine({
         ctx, 
@@ -195,7 +230,7 @@ function drawCrossLine(extendOptions){
         y0: y, 
         x1: ctx.canvas.width, 
         y1: y,
-        color,
+        style,
         lineWidth,
         globalCompositeOperation
     });
@@ -206,12 +241,53 @@ function drawCrossLine(extendOptions){
         y0: 0, 
         x1: x, 
         y1: ctx.canvas.height,
-        color,
+        style,
         lineWidth,
         globalCompositeOperation
     });
 }
-        
+/* 
+绘制网格
+*/
+function drawGrid(extendOptions){
+    let option = {
+        // ctx, 
+        stepX:10,
+        stepY:10
+    }
+    let {ctx, stepX, stepY, style, lineWidth,  globalCompositeOperation} = Object.assign({},option,extendOptions);
+    const w = ctx.canvas.width;
+    const h = ctx.canvas.height;
+    let x=0,
+        y=0;
+    while(x < w){
+        strokeLine({
+            ctx, 
+            x0: x, 
+            y0: 0, 
+            x1: x, 
+            y1: h,
+            style,
+            lineWidth,
+            globalCompositeOperation
+        });
+        x += stepX;
+    }
+    while(y < h){
+        strokeLine({
+            ctx, 
+            x0: 0, 
+            y0: y, 
+            x1: w, 
+            y1: y,
+            style,
+            lineWidth,
+            globalCompositeOperation
+        });
+        y += stepY;
+    }
+}
+
             
 export {
     setTheme,
@@ -221,5 +297,7 @@ export {
     drawRect,
     drawSector,
     drawCircle,
-    drawCrossLine
+    setShadowStyle,
+    drawCrossLine,
+    drawGrid
 };
